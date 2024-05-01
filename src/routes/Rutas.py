477203030e889806f080442f92de612ded4f38ed
Jl72ma72** <joe251217@gmail.com>
2024-04-30 19:04:29 -0500

    publicacion

--- src/routes/Rutas.py
+++ src/routes/Rutas.py
@@ -1,4 +1,4 @@
 # Ruta del archivo SQL
 ruta_Refacturaciones_Corporativas='./scripts/Refacturaciones_Corporativas.sql'
 ruta_env='.env'
-ruta_html='./output.html'
+ruta_html='.src/models/output.html'
